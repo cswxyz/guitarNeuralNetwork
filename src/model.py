3a3e84a757175739bdc26d04afbdab83de830d9f
Cedric <124837095+mylastdream@users.noreply.github.com>
2024-09-28 20:37:48 -0500

    Merge pull request #1
    * Cleaned up data
    
    * .
    
    * Cleaned up data
    
    * Merge remote-tracking branch 'origin/model_editing' into model_editing
    
    * removed dropout layer

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 
 def build_model(input_shape=(512, 512, 3)):
-    base_model = tf.keras.applications.MobileNetV2(input_shape=input_shape,
+    base_model = tf.keras.applications.Xception(input_shape=input_shape,
                                                    include_top=False,
                                                    weights='imagenet')
 
@@ -16,11 +16,9 @@ def build_model(input_shape=(512, 512, 3)):
 
     model.add(layers.BatchNormalization())
 
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(32, activation='relu'))
 
     model.add(layers.BatchNormalization())
-
-    model.add(layers.Dropout(0.3))
 
     model.add(layers.Dense(3, activation='softmax'))
 
