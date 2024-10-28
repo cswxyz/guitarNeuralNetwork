4812e166639f95dacd625f44408c16c2a7480390
mylastdream <cedricslavin@gmail.com>
2024-10-28 12:45:00 -0500

    made host changes for render, deleted azure logs

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import os
 from view.api import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Set the port to Render's specified PORT environment variable or default to 8080
+    port = int(os.environ.get("PORT", 8080))
+    # Ensure the app binds to 0.0.0.0 for Render
+    app.run(host="0.0.0.0", port=port, debug=True)
